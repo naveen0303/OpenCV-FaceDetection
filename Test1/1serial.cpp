b39ff3c8934fe361a19b9ce8ef64a8c52ef0a755
Naveen Milind Chalawadi <naveenchalawadi@gmail.com>
2020-01-20 19:02:43 +0530

    Update 1serial.cpp

--- Test1/1serial.cpp
+++ Test1/1serial.cpp
@@ -1,3 +1,4 @@
+/*
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -82,3 +83,4 @@ int main(int argc, char** argv)
     }
     return 0;
 }
+*/
