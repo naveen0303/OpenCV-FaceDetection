4f845570ac8e63fe70edc0884088c5e264055192
Naveen Milind Chalawadi <naveenchalawadi@gmail.com>
2020-01-20 19:03:33 +0530

    Update 3parallel.cpp

--- Test1/3parallel.cpp
+++ Test1/3parallel.cpp
@@ -1,3 +1,4 @@
+/*
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -99,3 +100,4 @@ int main(int argc, char** argv)
     }
     return 0;
 }
+*/
